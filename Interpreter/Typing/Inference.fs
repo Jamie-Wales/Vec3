75194f54bff2861be070706e63dc48259272fe19
Jacob Edwards <jacob.john1@talktalk.net>
2024-10-03 19:28:06 +0100

    type inference (some errors)

--- Interpreter/Typing/Inference.fs
+++ Interpreter/Typing/Inference.fs
@@ -3,41 +3,13 @@ module Vec3.Interpreter.Typing.Inference
 open Vec3.Interpreter.Grammar
 open Vec3.Interpreter.Token
 open Builtins
+open Exceptions
 
-type TType = Vec3.Interpreter.Grammar.Type
-
-type Substitution = Map<TypeVar, TType>
-type Scheme = Forall of TypeVar list * TType
 type TypeEnv = Map<string, TType>
 
+type Substitution = Map<TypeVar, TType>
+
 let defaultTypeEnv = BuiltinFunctions
-
-type TypeError =
-    | UndefinedVariable of Token
-    | UndefinedFunction of Token
-    | UndefinedType of Token
-    | TypeMismatch of Token * TType * TType
-    | InvalidAssignment of Token * TType * TType
-    | InvalidArgumentCount of Token * int * int
-    | InvalidArgumentType of Token * TType * TType
-    | InvalidReturnType of Token * TType * TType
-    | InvalidOperandType of Token * TType * TType
-    | InvalidOperator of Token * TType
-    | InvalidFunctionType of Token * TType
-    | InvalidFunction of Token * TType
-    | InvalidFunctionArgument of Token * TType * TType
-    | InvalidFunctionReturn of Token * TType * TType
-    | InvalidFunctionBody of Token * TType * TType
-    | InvalidBlock of Token * TType * TType
-    | InvalidCall of Token * TType
-    | InvalidCallType of Token * TType * TType
-    | InvalidCallReturn of Token * TType * TType
-    | InvalidCallBody of Token * TType * TType
-
-
-type TypeErrors = TypeError list
-
-exception TypeException of TypeErrors
 
 let combineMaps map1 map2 =
      Map.fold (fun acc key value -> Map.add key value acc) map2 map1
@@ -67,6 +39,7 @@ let checkIdentifier (env: TypeEnv) (token: Token) : Result<TType, TypeErrors> =
         | None -> Error [ TypeError.UndefinedVariable token ]
     | _ -> Error [ TypeError.UndefinedVariable token ]
 
+// attempts to substitute type variables with concrete types
 let rec applySubstitution (sub: Substitution) (t: TType) : TType =
     match t with
     | TTypeVariable tv ->
@@ -83,9 +56,11 @@ let rec applySubstitution (sub: Substitution) (t: TType) : TType =
     | TMatrix(typ, rows, cols) -> TMatrix(applySubstitution sub typ, rows, cols)
     | t -> t
 
+// attempts to substitute type variables with concrete types in an environment
 let applySubstitutionToEnv (sub: Substitution) (env: TypeEnv) : TypeEnv =
-    Map.map (fun _ t -> applySubstitution sub t) env
+    Map.map (fun _ -> applySubstitution sub) env
 
+// attempts to unify two types
 let rec unify (t1: TType) (t2: TType): Result<Substitution, TypeErrors> =
     match t1, t2 with
     | TInteger, TInteger
@@ -130,6 +105,46 @@ let rec unify (t1: TType) (t2: TType): Result<Substitution, TypeErrors> =
             | Error errors, Ok _ -> Error errors
             | Ok _, Error errors -> Error errors
             | Error errors1, Error errors2 -> Error(errors1 @ errors2)
+    | TConstrain(types1), TConstrain(types2) ->
+        if List.length types1 <> List.length types2 then
+            Error [ TypeError.TypeMismatch(Empty, t1, t2) ]
+        else
+            let results = List.map2 unify types1 types2 // assumes that the constraints are in the same order is this correct? and same length
+            List.fold(fun acc result ->
+                match acc, result with
+                | Ok sub1, Ok sub2 -> Ok (combineMaps sub1 sub2)
+                | Error errors, Ok _ -> Error errors
+                | Ok _, Error errors -> Error errors
+                | Error errors1, Error errors2 -> Error(errors1 @ errors2)) (Ok Map.empty) results
+        
+    | TConstrain(types), t
+    | t, TConstrain(types) ->
+        let validTypes = List.filter (fun typ -> unify typ t = Ok Map.empty) types
+        match validTypes with
+        | [] -> Error [ TypeError.TypeMismatch(Empty, t1, t2) ]
+        | [singleType] -> unify singleType t
+        | _ -> Ok Map.empty
+        
+    | TTuple types1, TTuple types2 ->
+        let results = List.map2 unify types1 types2
+        List.fold(fun acc result ->
+            match acc, result with
+            | Ok sub1, Ok sub2 -> Ok (combineMaps sub1 sub2)
+            | Error errors, Ok _ -> Error errors
+            | Ok _, Error errors -> Error errors
+            | Error errors1, Error errors2 -> Error(errors1 @ errors2)) (Ok Map.empty) results
+    
+    | TList typ1, TList typ2 -> unify typ1 typ2
+    | TVector(typ1, size1), TVector(typ2, size2) ->
+        if size1 = size2 then
+            unify typ1 typ2
+        else
+            Error [ TypeError.TypeMismatch(Empty, t1, t2) ]
+    | TMatrix(typ1, rows1, cols1), TMatrix(typ2, rows2, cols2) ->
+        if rows1 = rows2 && cols1 = cols2 then
+            unify typ1 typ2
+        else
+            Error [ TypeError.TypeMismatch(Empty, t1, t2) ]
     | _ -> Error [ TypeError.TypeMismatch(Empty, t1, t2) ]
 
 and occursCheck (tv: TypeVar) (t: TType) : bool =
@@ -140,13 +155,8 @@ and occursCheck (tv: TypeVar) (t: TType) : bool =
     | TList typ -> occursCheck tv typ
     | TVector(typ, _) -> occursCheck tv typ
     | TMatrix(typ, _, _) -> occursCheck tv typ
+    | TConstrain(types) -> List.exists (occursCheck tv) types
     | _ -> false
-
-let instantiate (scheme: Scheme) : TType =
-    match scheme with
-    | Forall(vars, typ) ->
-        let sub = Map.ofList (List.map (fun v -> v, freshTypeVar()) vars)
-        applySubstitution sub typ
 
 let rec freeTypeVars (typ: TType) : TypeVar list =
     match typ with
@@ -157,18 +167,13 @@ let rec freeTypeVars (typ: TType) : TypeVar list =
     | TList typ -> freeTypeVars typ
     | TVector(typ, _) -> freeTypeVars typ
     | TMatrix(typ, _, _) -> freeTypeVars typ
+    | TConstrain(types) -> List.collect freeTypeVars types // shouldnt have any type variables in constraints
     | _ -> []
 
 let freeTypeVarsInEnv (env: TypeEnv) : TypeVar list =
     env
     |> Map.toList
     |> List.collect (fun (_, typ) -> freeTypeVars typ)
-
-let generalise (env: TypeEnv) (typ: TType) : Scheme =
-    let freeVars = freeTypeVars typ |> Set.ofList
-    let envFreeVars = freeTypeVarsInEnv env |> Set.ofList
-    let generalVars = Set.difference freeVars envFreeVars |> Set.toList
-    Forall(generalVars, typ)
 
 // let defaultTypeEnv =
 //     List.fold (fun acc (name, typ) -> Map.add name (Forall([] ,typ)) acc) Map.empty BuiltinFunctions
@@ -188,14 +193,161 @@ let rec infer (env: TypeEnv) (expr: Expr) : Result<TType * Substitution, TypeErr
                                                             | Identifier name -> Map.add name typ acc
                                                             | _ -> acc) env paramList paramTypes
         let bodyResult = infer newEnv body
-        
         match bodyResult with
         | Ok (bodyType, sub) ->
             let paramTypes = List.map (applySubstitution sub) paramTypes
-            Ok (TFunction(paramTypes, bodyType), sub)
+            
+            if returnType = TInfer then
+                Ok (TFunction(paramTypes, bodyType), sub)
+            else
+                let returnResult = unify bodyType returnType
+                match returnResult with
+                | Ok sub -> Ok (TFunction(paramTypes, returnType), sub)
+                | Error errors -> Error errors
         | Error errors -> Error errors
-    | ECall(callee, args) -> failwith "todo!!!"
+        
+    | ECall(callee, args) ->
+        match callee with
+        | { lexeme = Identifier name } ->
+            match Map.tryFind name env with
+            | Some t ->
+                match t with
+                | TFunction(paramTypes, ret) ->
+                    let argResults = List.map (infer env) args
+                    if List.exists (fun result -> match result with | Error _ -> true | _ -> false) argResults then
+                        let errors = List.collect (fun result -> match result with | Error errors -> errors | _ -> []) argResults
+                        Error errors
+                    else
+                        let argResults = List.map (fun result -> match result with | Ok (t, sub) -> (t, sub) | _ -> failwith "Impossible") argResults
+                        let argTypes = List.map fst argResults
+                        let argSubs = List.map snd argResults
+                        
+                        let paramResults = List.map2 unify paramTypes argTypes
+                        if List.exists (fun result -> match result with | Error _ -> true | _ -> false) paramResults then
+                            let errors = List.collect (fun result -> match result with | Error errors -> errors | _ -> []) paramResults
+                            Error errors
+                        else
+                            let paramResults = List.map (fun result -> match result with | Ok sub -> sub | _ -> failwith "Impossible") paramResults
+                        
+                            let combinedSubs = List.fold combineMaps Map.empty paramResults
+                            let combinedSubs = List.fold combineMaps combinedSubs argSubs
+                        
+                            let returnType = applySubstitution combinedSubs ret
+                            Ok (returnType, combinedSubs)
+                | _ -> Error [ TypeError.InvalidCall(callee, t) ]
+            | None -> Error [ TypeError.UndefinedVariable callee ]
+        | _ -> Error [ TypeError.InvalidCall(callee, TInfer) ]
+                    
+    | EBinary(expr1, op, expr2) ->
+        let expr1Result = infer env expr1
+        let expr2Result = infer env expr2
+        
+        match expr1Result, expr2Result with
+        | Ok (t1, sub1), Ok (t2, sub2) ->
+            let sub = combineMaps sub1 sub2
+            let opType = match op.lexeme with
+                            | Operator Plus -> TConstrain([TInteger; TFloat; TRational; TComplex])
+                            | Operator Minus -> TConstrain([TInteger; TFloat; TRational; TComplex])
+                            | Operator Star -> TConstrain([TInteger; TFloat; TRational; TComplex])
+                            | Operator Slash -> TConstrain([TInteger; TFloat; TRational; TComplex])
+                            | Operator StarStar -> TConstrain([TInteger; TFloat; TRational; TComplex])
+                            
+                            | Operator EqualEqual -> TConstrain([TInteger; TFloat; TRational; TComplex; TString; TBool; TUnit])
+                            | Operator BangEqual -> TConstrain([TInteger; TFloat; TRational; TComplex; TString; TBool; TUnit])
+                            
+                            | Operator Less -> TConstrain([TInteger; TFloat; TRational; TComplex])
+                            | Operator LessEqual -> TConstrain([TInteger; TFloat; TRational; TComplex])
+                            | Operator Greater -> TConstrain([TInteger; TFloat; TRational; TComplex])
+                            | Operator GreaterEqual -> TConstrain([TInteger; TFloat; TRational; TComplex])
+                            
+                            | _ -> failwith "Invalid operator type"
+            
+            let opResult = unify opType t1
+            
+            match opResult with
+            | Ok sub' ->
+                let sub = combineMaps sub sub'
+                let opResult = unify t1 t2
+                match opResult with
+                | Ok sub'' ->
+                    let sub = combineMaps sub'' sub
+                    let returnT = applySubstitution sub t1
+                    Ok (returnT, sub)
+                | Error errors -> Error errors
+            | Error errors -> Error errors
+        | Error errors, _ -> Error errors
+        | _, Error errors -> Error errors
+    
+    | EUnary(op, expr) ->
+        let exprResult = infer env expr
+        match exprResult with
+        | Ok (t, sub) ->
+            let opType = match op.lexeme with
+                            | Operator Minus -> TConstrain([TInteger; TFloat; TRational; TComplex]) // maybe make this a type var
+                            // if i have a function (x, y) -> x + y, then x and y should be inferred to the same Constrain type, so that only one type can be passed in
+                            | Operator Plus -> TConstrain([TInteger; TFloat; TRational; TComplex])
+                            | Operator Bang -> TBool
+                            | _ -> failwith "todo!!!"
+            
+            let opResult = unify opType t
+            match opResult with
+            | Ok sub' ->
+                let sub = combineMaps sub sub'
+                let returnT = applySubstitution sub t
+                Ok (returnT, sub)
+            | Error errors -> Error errors
+        | Error errors -> Error errors
+    
+    | EBlock(stmts) -> failwith "todo!!!"
+    | EGrouping(expr) ->
+        infer env expr
         
     | _ -> failwith "todo"
 
+and inferStmt (env: TypeEnv) (stmt: Stmt) : Result<TypeEnv * Substitution, TypeErrors> =
+    match stmt with
+    | SExpression expr ->
+        let result = infer env expr
+        match result with
+        | Ok (_, sub) -> Ok (env, sub)
+        | Error errors -> Error errors
+    | SVariableDeclaration(name, typ, expr) ->
+        let result = infer env expr
+        match result with
+        | Ok (t, sub) ->
+            let typ = match typ with
+                        | TInfer -> t
+                        | _ -> typ
+            
+            let subResult = unify t typ
+            match subResult with
+            | Ok sub' ->
+                let sub = combineMaps sub sub'
+                let newEnv = match name with
+                                | { lexeme = Identifier name } -> Map.add name (applySubstitution sub typ) env
+                                | _ -> env
+                                
+                Ok (newEnv, sub)
+            | Error errors -> Error errors
+            
+        | Error errors -> Error errors
+    | SPrintStatement _ ->
+        Ok (env, Map.empty)
+
+and inferProgram (env: TypeEnv) (stmts: Stmt list) : Result<TypeEnv * Substitution, TypeErrors> =
+    List.fold (fun acc stmt ->
+        match acc with
+        | Ok (env, sub) ->
+            let result = inferStmt env stmt
+            match result with
+            | Ok (env', sub') ->
+                let sub = combineMaps sub sub'
+                Ok (env', sub)
+            | Error errors -> Error errors
+        | Error errors -> Error errors) (Ok (env, Map.empty)) stmts
+
+let quickInferStmt (env: TypeEnv) (stmt: Stmt) =
+    match inferStmt env stmt with
+    | Ok (env, _) -> env
+    | Error errors -> raise <| TypeException errors
 
