c4be1c35b5ca2104b1dde3719610d607f28eab84
Jamie Wales <jamie.wales.developer@gmail.com>
2024-11-25 13:22:15 +0000

    transpiling

--- untitled/vec3_math.h
+++
@@ -1,8 +0,0 @@
-
-#pragma once
-#include "value.h"
-
-Vec3Value* vec3_add(Vec3Value* a, Vec3Value* b);
-Vec3Value* vec3_subtract(Vec3Value* a, Vec3Value* b);
-Vec3Value* vec3_multiply(Vec3Value* a, Vec3Value* b);
-Vec3Value* vec3_divide(Vec3Value* a, Vec3Value* b);
