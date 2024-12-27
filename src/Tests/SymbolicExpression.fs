1b686818067ff8dd2df08f035401043f4174ce9c
Jacob Edwards <jacob.john1@talktalk.net>
2024-12-27 17:32:21 +0000

    report and tests

--- src/Tests/SymbolicExpression.fs
+++ src/Tests/SymbolicExpression.fs
@@ -1,9 +1,35 @@
 module Tests.SymbolicExpression
 
 open NUnit.Framework
+open Vec3.Interpreter.SymbolicExpression
 
-[<SetUp>]
-let Setup () = ()
+[<TestFixture>]
+type SymbolicExpressionTests() =
 
-[<Test>]
-let Test1 () = Assert.Pass()
+    [<Test>]
+    member _.Should_ConvertToString() =
+        let expr = Addition (Multiplication (X, Const 2), Const 3)
+        Assert.That("((x * 2) + 3)", Is.EqualTo(toString expr))
+    
+    [<Test>]
+    member _.Should_Evaluate() =
+        let expr = Addition (Multiplication (X, Const 2), Const 3)
+        Assert.That(7.0, Is.EqualTo( evaluate 2.0 expr))
+    
+    [<Test>]
+    member _.Should_Differentiate() =
+        let expr = Addition (Multiplication (X, Const 2), Const 3)
+        Assert.That(Const 2, Is.EqualTo(differentiate expr))
+    
+    [<Test>]
+    member _.Should_DifferentiateTwice() =
+        let expr = Addition (Multiplication (X, Const 2), Const 3)
+        Assert.That(Const 0, Is.EqualTo(differentiate (differentiate expr)))
+    
+    
+    [<Test>]
+    member _.Should_Integrate() =
+        let expr = Addition (X, Const 3)
+        Assert.That(Addition(Multiplication (Const 0.5, Multiplication (X, X)), Multiplication (Const 3.0, X)), Is.EqualTo(integrate expr))
+    
+    
--- src/Tests/TypeAnalysis/Inference.fs
+++ src/Tests/TypeAnalysis/Inference.fs
@@ -1,1 +1,123 @@
 module Tests.Typing.Inference
+
+open NUnit.Framework
+open Vec3.Interpreter.Token
+open Vec3.Interpreter.Typing.Inference
+open Vec3.Interpreter.Parser
+open Vec3.Interpreter.Grammar
+
+[<TestFixture>]
+type InferenceTests() =
+
+    [<Test>]
+    member _.Should_InferType() =
+        let expr = "let x = 2"
+        match parse expr false with
+        | Ok (_, program) ->
+            match inferProgram1 program with
+            | Ok (env, _, _, _) ->
+                Assert.That(TInteger, Is.EqualTo(Map.find (Identifier "x") env))
+            | Error _ -> Assert.Fail()
+        | Error _ -> Assert.Fail()
+        
+    [<Test>]
+    member _.Should_InferType2() =
+        let expr = "let x = 2 + 2"
+        match parse expr false with
+        | Ok (_, program) ->
+            match inferProgram1 program with
+            | Ok (env, _, _, _) ->
+                Assert.That(TInteger, Is.EqualTo(Map.find (Identifier "x") env))
+            | Error _ -> Assert.Fail()
+        | Error _ -> Assert.Fail()
+        
+    [<Test>]
+    member _.Should_InferType3() =
+        let expr = "let x = 2.0"
+        match parse expr false with
+        | Ok (_, program) ->
+            match inferProgram1 program with
+            | Ok (env, _, _, _) ->
+                Assert.That(TFloat, Is.EqualTo(Map.find (Identifier "x") env))
+            | Error _ -> Assert.Fail()
+        | Error _ -> Assert.Fail()
+        
+    [<Test>]
+    member _.Should_InferType4() =
+        let expr = "let x = 2.0 + 2.0"
+        match parse expr false with
+        | Ok (_, program) ->
+            match inferProgram1 program with
+            | Ok (env, _, _, _) ->
+                Assert.That(TFloat, Is.EqualTo(Map.find (Identifier "x") env))
+            | Error _ -> Assert.Fail()
+        | Error _ -> Assert.Fail()
+        
+    [<Test>]
+    member _.Should_InferType5() =
+        let expr = "let x = 2 + 2.0"
+        match parse expr false with
+        | Ok (_, program) ->
+            match inferProgram1 program with
+            | Ok (env, _, _, _) ->
+                Assert.That(TInteger, Is.EqualTo(Map.find (Identifier "x") env))
+            | Error _ -> Assert.Fail()
+        | Error _ -> Assert.Fail()
+        
+    [<Test>]
+    member _.Should_InferType6() =
+        let expr = "let x = (2 + 2.0) * 3"
+        match parse expr false with
+        | Ok (_, program) ->
+            match inferProgram1 program with
+            | Ok (env, _, _, _) ->
+                Assert.That(TInteger, Is.EqualTo(Map.find (Identifier "x") env))
+            | Error _ -> Assert.Fail()
+        | Error _ -> Assert.Fail()
+    
+    [<Test>]
+    member _.Should_InferType7() =
+        let expr = "let x = 2 + 2.0 * 3"
+        match parse expr false with
+        | Ok (_, program) ->
+            match inferProgram1 program with
+            | Ok (env, _, _, _) ->
+                Assert.That(TInteger, Is.EqualTo(Map.find (Identifier "x") env))
+            | Error _ -> Assert.Fail()
+        | Error _ -> Assert.Fail()
+        
+    [<Test>]
+    member _.Should_InferType8() =
+        let expr = "let x = (() -> 2)()"
+        match parse expr false with
+        | Ok (_, program) ->
+            match inferProgram1 program with
+            | Ok (env, _, _, _) ->
+                Assert.That(TInteger, Is.EqualTo(Map.find (Identifier "x") env))
+            | Error _ -> Assert.Fail()
+        | Error _ -> Assert.Fail()
+        
+    [<Test>]
+    member _.Should_InferType9() =
+        let expr = "let x = (() -> 2.0)()"
+        match parse expr false with
+        | Ok (_, program) ->
+            match inferProgram1 program with
+            | Ok (env, _, _, _) ->
+                Assert.That(TFloat, Is.EqualTo(Map.find (Identifier "x") env))
+            | Error _ -> Assert.Fail()
+        | Error _ -> Assert.Fail()
+        
+    [<Test>]
+    member _.Should_InferType10() =
+        let expr = "let x = (() -> 2 + 2.0)()"
+        match parse expr false with
+        | Ok (_, program) ->
+            match inferProgram1 program with
+            | Ok (env, _, _, _) ->
+                Assert.That(TInteger, Is.EqualTo(Map.find (Identifier "x") env))
+            | Error _ -> Assert.Fail()
+        | Error _ -> Assert.Fail()
+        
+    
+            
